5b276f0d129230555d678addd7a89babe92dc6e6
m4x0nus <max.dubiniuk+gh@gmail.com>
2022-06-10 14:11:45 +0300

    Added data validity check

---
+++ main.py
@@ -0,0 +1,19 @@
+config_name = "config.yaml"
+
+import yaml, os
+
+def Is_valid(config):
+    for skeleton in config["skeletons"]:
+        if not os.path.isdir(skeleton):
+            raise Exception('Skeleton path "' + skeleton + '" is not valid')
+    for i in config["out"]:
+        if not i["skeleton"] in config["skeletons"]:
+            raise Exception('Skeleton path "' + skeleton + '" is not in skeletons list')
+
+def main():
+    config = yaml.full_load(open(config_name, "r"))
+    print(config)
+    Is_valid(config)
+
+if __name__ == "__main__":
+    main()
