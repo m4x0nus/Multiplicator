c037749fa1608db771abfd7cb60b742cd5120455
m4x0nus <max.dubiniuk+gh@gmail.com>
2022-06-14 14:57:24 +0300

    Added click support

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
-config_name = "config.yaml"
+import click, os, yaml
 
-import yaml, os
+@click.command()
+@click.option("-c", "--config", default="config.yaml", help="Config yaml file path.", type=click.Path(exists=True))
+@click.option('--dry-try', is_flag=True, help="Just check the validity of the config.")
 
-def is_valid(config):
-    for skeleton in config["skeletons"]:
+def main(config, dry_try):
+    config_file = yaml.full_load(open(config, "r"))
+    is_valid(config_file)
+    if dry_try:
+        return
+
+def is_valid(config_file):
+    for skeleton in config_file["skeletons"]:
         if not os.path.isdir(skeleton):
             raise Exception('Skeleton path "' + skeleton + '" is not valid')
-    for i in config["out"]:
-        if not i["skeleton"] in config["skeletons"]:
+    for i in config_file["out"]:
+        if not i["skeleton"] in config_file["skeletons"]:
             raise Exception('Skeleton path "' + skeleton + '" is not in skeletons list')
-
-def main():
-    config = yaml.full_load(open(config_name, "r"))
-    print(config)
-    is_valid(config)
 
 if __name__ == "__main__":
     main()
