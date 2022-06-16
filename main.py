4b6a85550e9d3f603722d8c5e70c69c4a92fdaf9
m4x0nus <max.dubiniuk+gh@gmail.com>
2022-06-16 21:15:22 +0300

    make_backup, load_backup and erase_backup moved to separate functions

--- main.py
+++ main.py
@@ -4,30 +4,40 @@ import click, os, shutil, yaml
 @click.option("-c", "--config", default="config.yaml", help="Config yaml file path.", type=click.Path(exists=True))
 @click.option("--dry-run", is_flag=True, help="Just check the validity of the config.")
 
-def main(config, dry_try):
+def main(config, dry_run):
     config_file = yaml.full_load(open(config, "r"))
     is_valid(config_file)
-    if dry_try:
+    if dry_run:
         return
+    make_backup()
+    try:
+        for out in config_file["out"]:
+            shutil.copytree(os.path.join("skeletons", out["skeleton"]), os.path.join("out", list(out)[0]))
+    except Exception as exception:
+        load_backup()
+        print(exception)
+    else:
+        erase_backup()
+
+def make_backup():
     try:
         shutil.rmtree("out_backup")
     except FileNotFoundError:
         pass
-    os.rename(os.path.join("out"), os.path.join("out_backup"))
-    os.makedirs(os.path.dirname(os.path.join("out")), exist_ok=True)
-    try:
-        pass #Main part
-    except Exception as exception:
-        shutil.rmtree("out")
-        os.rename("out_backup", "out")
-        print(exception)
-    else:
-        shutil.rmtree("out_backup")
+    os.rename(os.path.join("", "out"), os.path.join("", "out_backup"))
+    os.makedirs("out", exist_ok=True)
+
+def load_backup():
+    shutil.rmtree("out")
+    os.rename("out_backup", "out")
+
+def erase_backup():
+    shutil.rmtree("out_backup")
 
 def is_valid(config_file):
     for skeleton in config_file["skeletons"]:
-        if not os.path.isdir(skeleton):
-            raise Exception('Skeleton path "' + skeleton + '" is not valid')
+        if not os.path.isdir(os.path.join("skeletons", skeleton)):
+            raise Exception('Skeleton path "' + os.path.join("skeletons", skeleton) + '" is not valid')
     for i in config_file["out"]:
         if not i["skeleton"] in config_file["skeletons"]:
             raise Exception('Skeleton path "' + skeleton + '" is not in skeletons list')
